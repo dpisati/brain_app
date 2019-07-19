c33f99107b9c9eca86eb66429b95298fb3dd1d96
Daniel Pisati <dpisati@gmail.com>
2019-07-19 14:44:44 +1200

    First

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
+class App extends Component {
+  render (){ 
+    return (
+      <div className="App">
+          <Particles className='particles' 
+          params={particlesOption} />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          {/* 
+          <FaceRecognition /> */}
+
+      </div>
+    );
+  }
+}
+
+export default App;
