0002fcbdea3ca330a0156270e4ce7440b1c593e2
Daniel Pisati <dpisati@gmail.com>
2019-07-22 14:23:31 +1200

    clicksubmit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,21 @@ const particlesOption = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render (){ 
     return (
       <div className="App">
@@ -29,7 +44,9 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}/>
           {/* 
           <FaceRecognition /> */}
 
