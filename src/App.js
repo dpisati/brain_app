b92f5f94756dcf3212fad505f53ae216c2965cbb
Daniel Pisati <dpisati@gmail.com>
2019-07-24 21:04:58 +1200

    signin/register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -32,6 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -65,18 +69,37 @@ class App extends Component {
         .catch(err => console.log(err));
   }
 
-  render (){ 
+  onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render () {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
           <Particles className='particles' 
-          params={particlesOption} />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-            onInputChange={this.onInputChange} 
-            onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            params={particlesOption} />
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+          { route === 'home' 
+              ? <div>
+                  <Logo />
+                  <Rank />
+                  <ImageLinkForm 
+                    onInputChange={this.onInputChange} 
+                    onButtonSubmit={this.onButtonSubmit}/>
+                  <FaceRecognition box={box} imageUrl={imageUrl}/>
+                </div>
+              : (
+                route === 'signin'
+                ? <SignIn onRouteChange={this.onRouteChange} />
+                : <Register onRouteChange={this.onRouteChange} />
+                )
+          }             
       </div>
     );
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const Navigation = () => {
-    return(
-        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+const Navigation = ({onRouteChange, isSignedIn}) => {
+        if(isSignedIn) {
+            return (
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
             <nav>
-                <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+                <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
             </nav>
-        </div>
-    );
+            </div>
+            );
+        } else {
+            return (
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <nav>
+                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+            </nav>
+            </div>
+            );
+        }
 }
 
 export default Navigation;
