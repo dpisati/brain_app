f884fbc19f0269c35d1390d4bac4c1a1018b695d
dpisati <43629737+dpisati@users.noreply.github.com>
2019-12-20 17:31:10 +1300

    res

--- smart_brain_api/server.js
+++ smart_brain_api/server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send("its working!");
 });
 app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
