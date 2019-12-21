57ea872fc319e6fc32480770273e7e87f8e7c73c
dpisati <43629737+dpisati@users.noreply.github.com>
2019-12-22 10:56:37 +1300

    http

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => {
         if (response) {
-          fetch("https://dpist-smart-brain.herokuapp.com/image", {
+          fetch("http://dpist-smart-brain.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("https://dpist-smart-brain.herokuapp.com/register", {
+    fetch("http://dpist-smart-brain.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
